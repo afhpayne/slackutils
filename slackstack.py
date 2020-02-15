7c28d1f4b95452006aa1ad80b0b8f85ce2e546cb
afhpayne <github@komputermatrix.com>
2020-02-15 10:22:56 -0500

    update slackstack 0.8.4

--- slackstack.py
+++ slackstack.py
@@ -5,7 +5,7 @@ soft_name = "Slackstack"
 soft_tag  = "a slackbuild utility"
 
 # Version
-soft_vers = "0.8.3"
+soft_vers = "0.8.4"
 
 import glob
 import os
@@ -64,7 +64,6 @@ else:
         print("Please create a local clone to use slackstack")
         exit(1)
 
-
 check_path = glob.glob(os.path.join(os.environ['HOME'], "slackstack", "*tree", ""))
 if check_path:
     check_path = (check_path[0].strip())
@@ -82,26 +81,13 @@ prog_build_dir = (prog_name + "-tree")
 dir_path = os.path.join(os.environ['HOME'], "slackstack", prog_build_dir, "")
 print("\nBuild directory =", dir_path)
 
-dirs = ['academic',
-        'accessibility',
-        'audio',
-        'business',
-        'desktop',
-        'development',
-        'games',
-        'gis',
-        'graphics',
-        'ham',
-        'haskell',
-        'libraries',
-        'misc',
-        'multimedia',
-        'network',
-        'office',
-        'perl',
-        'python',
-        'ruby',
-        'system']
+# Make a list of SBo software categories
+sbo_dirs = []
+for name in os.listdir(dir_git):
+    if os.path.isdir(os.path.join(dir_git, name)) is True:
+        if "." not in name:
+            sbo_dirs.append(name.strip())
+sbo_dirs.sort()
 
 list1_checked_for_deps = []
 list2_is_a_dep = []
@@ -109,16 +95,14 @@ list3_install_seq = []
 
 path_to_prog = os.path.join(dir_personal + prog_name)
 if os.path.isdir(path_to_prog):
-    # print("\nFound", os.path.join(path_to_prog))
     try:
         shutil.copytree(os.path.join(path_to_prog), os.path.join(dir_path, prog_name))
     except FileExistsError:
         pass
 else:
-    for dir in dirs:
+    for dir in sbo_dirs:
         path_to_prog = (dir_git + dir + "/" + prog_name)
         if os.path.isdir(path_to_prog):
-            # print("\nFound:", os.path.join(path_to_prog))
             try:
                 shutil.copytree(os.path.join(path_to_prog), os.path.join(dir_path, prog_name))
             except FileExistsError:
@@ -149,9 +133,7 @@ def get_version_of_dep():
             if "VERSION=" in line:
                 version_dep = line.strip()
                 version_dep = version_dep.split('"')[1]
-                
-                
-#list1_checked_for_deps.append(prog_name)
+    
 def iterate_for_dependecies():
     for dir in os.listdir(os.path.join(dir_path)):
         prog_name = dir
@@ -159,12 +141,9 @@ def iterate_for_dependecies():
 
         if prog_name != "" and prog_name not in list1_checked_for_deps:
             list1_checked_for_deps.append(prog_name)
-#            print(prog_name)
-
             infofile = glob.glob(os.path.join(dir_path, prog_name, "*.info"))
             if infofile:
                 infofile = infofile[0]
-
                 with open(infofile) as i:
                     infofile = i.read()
                 i.closed
@@ -173,37 +152,25 @@ def iterate_for_dependecies():
                     if "REQUIRES=" in line:
                         depends = line.strip()
                         depends = depends.split('"')[1]
-#                        print("\nThis software requires:")
                         for dep in depends.split(" "):
-#                            print("-->", dep)
-                            #if dep not in list2_is_a_dep:
                             list2_is_a_dep.append(dep)
 
                 for item in list2_is_a_dep:
                     prog_name = item
                     path_to_prog = os.path.join(dir_personal + prog_name)
-#                    print(path_to_prog)
                     if os.path.isdir(path_to_prog):
-#                        print(os.path.join(path_to_prog))
-#                        print("YES")
                         try:
                             shutil.copytree(os.path.join(path_to_prog), os.path.join(dir_path, prog_name))
                         except FileExistsError:
                             pass
-#                         #if prog_name not in list1_checked_for_deps:
-#                         list1_checked_for_deps.append(prog_name)
                     else:
-                        for dir in dirs:
+                        for dir in sbo_dirs:
                             path_to_prog = os.path.join(dir_git + dir + "/" + prog_name)
                             if os.path.isdir(path_to_prog):
- #                               print(os.path.join(path_to_prog))
- #                               print("YES")
                                 try:
                                     shutil.copytree(os.path.join(path_to_prog), os.path.join(dir_path, prog_name))
                                 except FileExistsError:
                                     pass
-                                #if prog_name not in list1_checked_for_deps:
-#                                 list1_checked_for_deps.append(prog_name)
 
 def iterate_for_permissions():
     for dir in os.scandir(os.path.join(dir_path)):
@@ -302,7 +269,7 @@ for dep in list3_install_seq:
                     print(dep_status)                
                     print("")
                     f.write(dep + " " + version_dep + " " + dep_status + "\n")
-                else:# package_dict.get(dep) is None and python_dict.get(dep) is None:
+                else:
                     get_version_of_dep()
                     print(dep + " " + version_dep)
                     print("")
@@ -316,12 +283,12 @@ for dep in list3_install_seq:
                 print(dep_status)                
                 print("")
                 f.write(dep + " " + version_dep + " " + dep_status + "\n")
-            else:# package_dict.get(dep) is None and python_dict.get(dep) is None:
+            else:
                 get_version_of_dep()
                 print(dep + " " + version_dep)
                 print("")
                 f.write(dep + " " + version_dep + "\n")
-        else:# package_dict.get(dep) is None and python_dict.get(dep) is None:
+        else:
             get_version_of_dep()
             print(dep + " " + version_dep)
             print("")
@@ -329,3 +296,10 @@ for dep in list3_install_seq:
 f.close()
 
 print("\nto", dir_path, "\n")
+
+grab_y_n = input("Run slackgrab.py to get the tarballs (y/n)? ")
+if grab_y_n == "Y" or grab_y_n == "y":
+    subprocess.run(["slackgrab.py"])
+    exit(0)
+else:
+    exit(0)
