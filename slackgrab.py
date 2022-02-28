87ed6f85acd02028f429dba3bdda101f7c54381c
afhpayne <github@komputermatrix.com>
2022-02-28 11:13:43 -0500

    update

--- slackgrab.py
+++ slackgrab.py
@@ -2,7 +2,7 @@
 
 # MIT License
 
-# Copyright (c) 2019-2022 Andrew Payne
+# Copyright (c) 2019-2020 Andrew Payne
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
